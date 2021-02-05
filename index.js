dca24d050336a4fa9688910cb26e1fed056fedb8
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-05 17:22:04 -0500

    commit tree d1356c30c2e5ec56aa2a789848f387744ba2f6ad

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(collection, query){
+  return collection.filter(element => {return element.toLowerCase() === query.toLowerCase()});
+};
+
+function fuzzyMatch(collection, query){
+  return collection.filter(element => {
+    if(element.substring(0, query.length).match(query)){
+      return element;
+    };
+  });
+};
+
+function matchName(driverCollection, query){
+  return driverCollection.filter(driver =>  {return driver.name === query})
+}
